f7fa597e963f0f88477856e1e6ece539357ac2bc
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2024-03-14 21:28:28 +0100

    fix persist store bug

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,15 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-//Reducer
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import {AuthReducer} from "./auth/index.js";
 
 const rootReducer = combineReducers({
@@ -10,14 +18,20 @@ const rootReducer = combineReducers({
 
 const persistConfig = {
     key: 'root',
+    version: 1,
     storage,
-};
+}
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
     reducer: persistedReducer,
-    devTools: true
-});
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
 
-export const persistedStore = persistStore(store);
+export const persistedStore = persistStore(store)
