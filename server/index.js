0602b6a06ef1a176cd0ac2e951753eb3ae2e53a0
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2024-03-16 00:16:38 +0100

    implement endpoint: all applications

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 //router
 import userRouter from "./router/users/route.js";
+import appRouter from "./router/applications/route.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 app.use(userRouter);
+app.use(appRouter)
 
 app.listen(3000, () => {
     console.log("The server is running on port 3000.");
---
+++ server/router/applications/route.js
@@ -0,0 +1,39 @@
+import express from "express";
+import {PrismaClient} from "@prisma/client";
+
+const router = express.Router();
+
+const prisma = new PrismaClient();
+
+router.get('/applications', async (req, res) => {
+    try{
+        const categoriesWithApps = await prisma.app_categories.findMany({
+            include: {
+                applications: true
+            }
+        });
+
+        const formattedData = categoriesWithApps.map(category => {
+            return {
+                category: {
+                    id: category.categorie_id,
+                    name: category.categorie_name
+                },
+                apps: category.applications.map(app => {
+                    return {
+                        app_id: app.app_id,
+                        app_name: app.app_name
+                    };
+                })
+            };
+        });
+
+        return res.send({data: formattedData});
+    }catch (error){
+        return res.status(500).send({message: error})
+    }finally {
+        await prisma.$disconnect();
+    }
+})
+
+export default router;
