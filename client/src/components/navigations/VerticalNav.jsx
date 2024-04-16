80a1a7cb6857b67b1dbd0c2d2eb9480fda8914d3
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2024-04-16 11:26:59 +0200

    Add tags in the vertical nav and a little refactor

--- client/src/components/navigations/VerticalNav.jsx
+++ client/src/components/navigations/VerticalNav.jsx
@@ -1,71 +1,14 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import Icon from "../Icon.jsx";
 import allIcons from "../../utils/allIcons.js";
 import {useSelector} from "react-redux";
 import {currentUserSelector} from "../../redux/auth/selector.js";
 import {allAppsNoCatSelector} from "../../redux/app/selector.js";
 import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
 
-
-const ShortcutsExtend = () => {
-
-    const allApps = useSelector(allAppsNoCatSelector);
-    const {shortcuts} = useSelector(currentUserSelector);
-
-    const getAllApps = () => {
-        let res = [];
-        for(let i = 0; i < shortcuts.length; i++){
-            for(let j = 0; j < allApps.length; j++){
-                if(!res.includes(allApps[j]) && allApps[j].app_id === shortcuts[i].app_id){
-                    res.push(allApps[j]);
-                }
-            }
-        }
-        return res;
-    }
-
-    return shortcuts.length > 0 &&
-        <div className="vertical-nav-tab-extends-container">
-            {
-                getAllApps().map((app, idx) => {
-                    return(
-                        <div key={idx}>
-                            <Link to={`/user/software/${app.app_id}`}>
-                                <span>
-                                    {app.app_name}
-                                </span>
-                            </Link>
-                        </div>
-                    )
-                })
-            }
-        </div>
-}
-
-
-const tabs = [
-    {
-        name: "Profile",
-        icon: allIcons.app,
-    },
-    {
-        name: "Settings",
-        icon: allIcons.home,
-    },
-    {
-        name: "Software",
-        icon: allIcons.keyboard,
-        extends: <ShortcutsExtend />
-    },
-    {
-        name: "Classement",
-        icon: allIcons.points,
-    }
-]
-
-const VerticalNavTabs = () => {
-
-    const [extend, setExtend] = useState(false);
+const Tab = (props) => {
+    const { tab, idx, extend, action, tag } = props;
 
     const borderStyle = (idx) => {
         const borderType = "2px solid #E5E7EB";
@@ -98,27 +41,97 @@ const VerticalNavTabs = () => {
         return res;
     };
 
+    return  <div className="vertical-nav-tab" style={borderStyle(idx)} onClick={action}>
+        <div className="vertical-nav-tab-content">
+            <div>
+                <Icon path={tab.icon} />
+                <span>{tab.name}</span>
+            </div>
+            { tag && <div className="tag">{tag}</div> }
+        </div>
+        {extend}
+    </div>
+}
+
+Tab.propTypes = {
+    tab: PropTypes.object.isRequired,
+    idx: PropTypes.number.isRequired,
+    extend: PropTypes.element,
+    action: PropTypes.func,
+    tag: PropTypes.number
+}
+
+const ShortcutsExtend = (props) => {
+    const {tab, idx} = props;
+    const [extend, setExtend] = useState(false);
+
+    const allApps = useSelector(allAppsNoCatSelector);
+    const {shortcuts} = useSelector(currentUserSelector);
+
+    const getAllUserApps = () => {
+        const userAppIds = shortcuts.map(shortcut => shortcut.app_id);
+        const uniqueUserAppIds = Array.from(new Set(userAppIds));
+
+        return allApps.filter(app => uniqueUserAppIds.includes(app.app_id));
+    }
+
+    const allUserApp = useMemo(() => getAllUserApps(), [allApps]);
+
+    return extend && shortcuts.length > 0 ?
+        <Tab tab={tab} idx={idx}  action={() => setExtend(false)} tag={allUserApp.length} extend={
+            <div className="vertical-nav-tab-extends-container">
+                {
+                    allUserApp.map((app, idx) => {
+                        return(
+                            <div key={idx}>
+                                <Link to={`/user/software/${app.app_id}`}>
+                                    <span>
+                                        {app.app_name}
+                                    </span>
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        }/> : <Tab tab={tab} idx={idx} action={() => setExtend(true)} tag={allUserApp.length}/>
+}
+
+ShortcutsExtend.propTypes = {
+    tab: PropTypes.object,
+    idx: PropTypes.number
+}
+
+
+const tabs = [
+    {
+        name: "Profile",
+        icon: allIcons.app,
+    },
+    {
+        name: "Settings",
+        icon: allIcons.home,
+    },
+    {
+        name: "Software",
+        icon: allIcons.keyboard,
+        extend: <ShortcutsExtend />
+    },
+    {
+        name: "Classement",
+        icon: allIcons.points,
+    }
+]
+
+const VerticalNavTabs = () => {
+
     return tabs.map((tab, idx) => (
         <React.Fragment key={idx}>
-            {!tab?.extends ?
-                <div className="vertical-nav-tab" style={borderStyle(idx)}>
-                    <div className="vertical-nav-tab-content">
-                        <Icon path={tab.icon} />
-                        <span>{tab.name}</span>
-                    </div>
-                </div>
-                :
-                <div className="vertical-nav-tab" style={borderStyle(idx)}>
-                    <div className="vertical-nav-tab-content" onClick={() => setExtend(!extend)}>
-                        <Icon path={tab.icon} />
-                        <span>{tab.name}</span>
-                    </div>
-                    {extend &&
-                        <>
-                            {tab.extends}
-                        </>
-                    }
-                </div>
+            {tab?.extend ?
+                <>
+                    {React.cloneElement(tab.extend, {tab: tab, idx: idx})}
+                </>
+                : <Tab tab={tab} idx={idx} />
             }
         </React.Fragment>
     ));
